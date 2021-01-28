92c83e78074ed65c60d2cb1f3be1e9e723dda896
ozckarr <lindbladoskar@gmail.com>
2021-01-28 08:46:34 +0100

    proxy

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://stark-cliffs-14495.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
